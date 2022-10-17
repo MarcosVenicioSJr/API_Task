a4f4cc44a2d0633a4eff52ef5519375704ee7eb7
Marcos Venicio <marcos.mendes@fitbank.com.br>
2022-10-17 10:32:33 -0300

    TaskContext Criada. Criando a conexão com BD

---
+++ API_Task/Context/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_Task.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Task.Context
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskModels> tasks { get; set; }
+    }
+}

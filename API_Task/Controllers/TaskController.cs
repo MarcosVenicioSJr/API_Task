bd65a76697b8cbc003f0a5ab7f152298beea5ece
Marcos Venicio <marcos.mendes@fitbank.com.br>
2022-10-17 12:49:10 -0300

    Criando método de criação de tarefas

---
+++ API_Task/Controllers/TaskController.cs
@@ -0,0 +1,28 @@
+﻿using API_Task.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Task.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpPost("Create")]
+        public IActionResult Create(TaskModels task)
+        {
+
+        }
+
+
+        [HttpGet("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            return Ok();
+        }
+    }
+}

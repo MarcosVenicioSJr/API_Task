4f47a385c44c270e8278b15b81d3bb9c546c0e59
Marcos Venicio <marcos.mendes@fitbank.com.br>
2022-10-17 10:28:20 -0300

    Model criada. Criando Enum de status

---
+++ API_Task/Models/TaskModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Task.Models
+{
+    public class TaskModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public EnumTask Status { get; set; }
+
+    }
+}
